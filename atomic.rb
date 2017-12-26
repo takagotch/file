754b6f585c6f33c013e958ccbd46098360348bc6
takagotch <dyaccb@gmail.com>
2017-12-26 10:20:00 +0900

    FileUtils

---
+++ atomic.rb
@@ -0,0 +1,12 @@
+require 'tempfile'
+require 'fileutils'
+temp =Tempfile.new("working")
+File.foreach(ARGV[0]) do |line|
+  #comennt*#* unless
+  temp << line unless line =~ /^\s*#/
+end
+
+temp.close
+FileUtils.cp(ARGV[0], "#{ARGV[0]}.bak")
+FileUtils.mv(temp.path, ARGV[0])
+
